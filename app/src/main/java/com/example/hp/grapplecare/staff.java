69bdeda51e4a5b7486fefd289d6874105378a276
Haris <hrz246@gmail.com>
2015-02-06 17:14:48 +0530

    staff.java  updated

--- app/src/main/java/com/example/hp/grapplecare/staff.java
+++ app/src/main/java/com/example/hp/grapplecare/staff.java
@@ -25,6 +25,7 @@ public class staff extends Activity {
     String tickets[];
     String sta[];
     String id[];
+    
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
